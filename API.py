13d9148da85c1760313fcd9e8b2c209a5fda8555
marihanson <hmarianne1@yahoo.com>
2022-06-30 15:46:06 +0200

    implement an API reader function

---
+++ API.py
@@ -0,0 +1,11 @@
+#API reader
+
+import requests
+
+def get_phone(name):
+    r = requests.get(f'http://localhost:5000/api?action=phone&name={name}')
+    return r.text
+
+def get_name(phone):
+    r = requests.get(f'http://localhost:5000/api?action=name&phone={phone}')
+    return r.text
